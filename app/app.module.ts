58765c58c2f823020310929ce0fe72a01e3557f9
Pavol Pitonak <ppitonak@redhat.com>
2016-10-17 14:01:20 +0200

    dashboard component and basic routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
@@ -10,10 +12,26 @@ import { HeroService }         from './hero.service';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent
     HeroDetailComponent,
     HeroesComponent
   ],
