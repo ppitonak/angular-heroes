6c76d1383eff8202270a538c644e995e59357935
Pavol Pitonak <ppitonak@redhat.com>
2016-10-18 14:46:51 +0200

    mock data replaced with mock http server, create/update/delete implemented

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }        from './app.component';
 import { DashboardComponent }  from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService }         from './hero.service';
 
 import { AppRoutingModule }    from './app-routing.module';
@@ -14,6 +19,8 @@ import { AppRoutingModule }    from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
