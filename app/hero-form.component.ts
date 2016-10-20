befd2bac136b4b0f2de95f84b50a3cca53d2d13a
Pavol Pitonak <ppitonak@redhat.com>
2016-10-20 16:51:40 +0200

    hero form implemented

--- app/hero-form.component.ts
+++ app/hero-form.component.ts
@@ -13,8 +13,15 @@ export class HeroFormComponent {
 
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
   submitted = false;
+  active = true;
 
   onSubmit() { this.submitted = true; }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+  }
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
